143568e9f3ab4684b2f439bc8ef2a8cfeb6e89ab
trinhnguyen212 <tngu212@gmail.com>
2023-08-10 16:23:02 +1200

    Draw a Triangle

---
+++ TrianglePattern/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrianglePattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 8;
+            int i, j, k;
+            
+            for (i = 1; i <= value; i++)
+            {
+                for (j = 1; j <= value - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (k =1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
